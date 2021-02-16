18bc8d55de03c1487b3f0b6c1b8ab1fadd1c566f
이승준 <ryan@dongwon.com>
2021-02-16 22:14:43 +0900

    first commit

---
+++ src/main/java/com/study/project/RyanprojectApplication.java
@@ -0,0 +1,13 @@
+package com.study.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/project/RyanprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
